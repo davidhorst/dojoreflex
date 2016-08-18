7bf225c426fcddf902560c21b601b26f5dd78a06
perkinsb01 <spencer@spencerperkins.net>
2016-08-18 09:31:39 -0700

    added controllers

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,10 @@
+class InstructorsController < ApplicationController
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'instructors/edit'
+
+  get 'instructors/new'
+
+  get 'instructors/show'
+
+  get 'students/new'
+
+  get 'students/show'
+
+  get 'students/edit'
+
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
