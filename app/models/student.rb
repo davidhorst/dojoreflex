ce1f8e0617f456f515f82f7dd8c4e676795d3bdd
perkinsb01 <spencer@spencerperkins.net>
2016-08-18 10:47:50 -0700

    added validations for the avatar type

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,7 @@ class Student < ActiveRecord::Base
   has_many :student_stacks
   has_many :stacks, through: :student_stacks
   has_secure_password
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
