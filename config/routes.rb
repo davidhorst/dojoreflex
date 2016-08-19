17fef79b0b898594b574a70ffa01f956dbaaddf8
David Horst <David Horst>
2016-08-19 09:50:52 -0700

    directory html

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
 
   get 'students/edit'
 
-  
+  get 'directory' => 'directory#index'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
