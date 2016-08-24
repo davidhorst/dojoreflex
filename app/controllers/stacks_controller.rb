bd673d7b372bd04f9118170f7c1bac095ae8e570
David Horst <David Horst>
2016-08-24 10:24:38 -0700

    navigation

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -1,6 +1,13 @@
 class StacksController < ApplicationController
 
+  def index
+    @user = current_instructor
+    @stacks = Stack.all
+  end
+
   def show
+    @user = current_instructor
+    @stack = Stack.find(params[:id])
     @students = Stack.find(params[:id]).stack_students
   end
 
