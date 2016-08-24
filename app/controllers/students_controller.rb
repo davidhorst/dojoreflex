2985a95fc2f038cefae2af47a59ef8cf9c8cffdc
perkinsb01 <spencer@spencerperkins.net>
2016-08-23 19:25:35 -0700

    tweaked color scheme of sliders

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,8 +15,6 @@ class StudentsController < ApplicationController
         @weekcount = Date.today.strftime("%U").to_i - current_student.cohort.start.strftime("%U").to_i
         @language = current_student.stacks.where(active:true).first.language.name
         @students = Student.all
-        
-
     end
 
     def new
