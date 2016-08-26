322e32f3719ec4cac83d7bbcfd12329febefb064
David Horst <David Horst>
2016-08-26 10:01:36 -0700

    asssignments upgrde

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -5,5 +5,7 @@ class Stack < ActiveRecord::Base
   has_many :belts
   has_many :stack_students
   has_many :students, through: :stack_students
-  has_many :assignments, class_name: :StackAssignment
+  has_many :assignments, foreign_key: "stack_id", class_name: "StackAssignment"
+  has_many :stack_assignments
+
 end
